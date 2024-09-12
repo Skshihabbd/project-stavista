3dd6ccb9e815a1c134e6d94d5171d2282a1964d3
unknown <skshihab170@gmail.com>
2024-09-12 18:39:28 +0600

    date-range

--- client/src/components/Form/AddRoomForm.jsx
+++ client/src/components/Form/AddRoomForm.jsx
@@ -1,4 +1,5 @@
 import { categories } from "../Categories/CategoriesData";
+import { DateRangePicker } from "react-date-range";
 const AddRoomForm = () => {
   return (
     <div className="w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50">
